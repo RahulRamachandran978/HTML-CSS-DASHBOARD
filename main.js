219a6ac47a0a6334da969f9dfe4d76ea95b61ca5
RahulRamchandran978 <rahulramchandran978@gmail.com>
2024-08-06 11:30:33 +0530

    form submittion

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const search = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const uploadButton = document.querySelector('.img_upload button');
 const fileInput = document.querySelector('.img_upload input[type="file"]');
+const button = document.getElementById('btn')
+const fileUpload = document.getElementById('file-upload');
+const myForm = document.getElementById('myForm');
+
+
+
 
 
 
@@ -29,4 +35,14 @@ document.addEventListener('DOMContentLoaded',()=> {
     } else {
         console.error('Upload button or file input not found');
     }
+});
+
+
+button.addEventListener('click',()=>{
+    fileUpload.click();
+});
+
+myForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    // let isValid = true;
 });
